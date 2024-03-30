3d25322464a93946810ecf1530ecfd539621bc97
Muhwezi <michealphenh10@gmail.com>
2024-03-31 02:22:11 +0300

    Handles wrong files entered

--- monty.c
+++ monty.c
@@ -7,5 +7,21 @@
  **/
 int main(int argc, char *argv[])
 {
+	FILE *fp = NULL;
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(fp);
 	exit(EXIT_SUCCESS);
 }
